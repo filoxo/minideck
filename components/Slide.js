6c41b86b46704fc287ced43b2cfa806c58c04e78
Carlos Filoteo <carlos.filoteo@podium.com>
2019-11-29 14:13:06 -0700

    Remove unneeded context and index checking

--- components/Slide.js
+++ components/Slide.js
@@ -2,18 +2,16 @@ import React, { useRef } from "react";
 import styles from "./Slide.css";
 import useAppearSequence from "./useAppearSequence";
 
-export default function Slide({ children, index, ...props }) {
-  const { currentSlideIndex } = useDeckContext();
-  const isCurrentStep = currentSlideIndex === index;
+export default function Slide({ children, isActive, ...props }) {
   const slideRef = useRef();
 
-  useAppearSequence(slideRef, isCurrentStep);
+  useAppearSequence(slideRef, isActive);
 
   return (
     <section
       ref={slideRef}
       className={styles.Slide}
-      aria-current={isCurrentStep ? "step" : null}
+      aria-current={isActive ? "step" : null}
       {...props}
     >
       <div className={styles.SlideContent}>{children}</div>
