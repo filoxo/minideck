68b8d5c8e5fbebb3aca3f1a062a11f53a66ffa0f
Carlos Filoteo <carlos.filoteo@podium.com>
2019-11-28 17:51:27 -0700

    Remove extra Appear component code

--- Appear.js
+++ Appear.js
@@ -1,37 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { useAppearContext, useSlideContext } from "./contexts";
+import React from "react";
 import styles from "./Appear.css";
 
 export default function Appear({ children }) {
-  const slideIndex = useSlideContext();
-  const appearState = useAppearContext();
-
-  const { register } = appearState;
-  const currentAppearIndex = appearState.slide[slideIndex]
-    ? appearState.slide[slideIndex].current
-    : -1;
-
-  const [indexToAppearAt, setIndexToAppearAt] = useState(null);
-
-  useEffect(() => {
-    if (slideIndex !== null && indexToAppearAt === null) {
-      const registeredIndex = register(slideIndex);
-      console.log("registeredIndex", registeredIndex);
-      setIndexToAppearAt(registeredIndex);
-    }
-  }, [slideIndex, register, indexToAppearAt, setIndexToAppearAt]);
-
-  return React.Children.map(children, child => {
-    const ogClassName = child.className || "";
-    const className =
-      currentAppearIndex > indexToAppearAt
-        ? ogClassName
-        : ogClassName + " " + styles.hidden;
-
-    const updatedChild = React.cloneElement(child, {
-      ...child.props,
-      className
-    });
-    return updatedChild;
-  });
+  return (
+    <span className={styles.Appear} data-appear="false">
+      {children}
+    </span>
+  );
 }
