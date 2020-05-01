a88ef54084be4c2de4bab5c5ca5d9c6218110f79
Carlos Filoteo <c.filoteo12@gmail.com>
2020-05-01 13:54:25 -0600

    Add Tailwind classes to Markdown components

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import cls from "classnames";
 import { MDXProvider } from "@mdx-js/react";
 
 import Deck from "./components/Deck";
 import Slide from "./components/Slide";
 import Appear from "./components/Appear";
 import Code from "./components/Code";
-import Table from "./components/Table";
 
 import MDXContent from "./content.mdx";
+
+const Base = (C, classnames) => (props) => (
+  <C {...props} className={cls(classnames, props.className)} />
+);
 
 render(
   <MDXProvider
@@ -17,10 +21,13 @@ render(
       Slide,
       Appear,
       code: Code,
-      table: Table,
-      h1: (props) => <h1 className="text-3xl" {...props} />,
-      h2: (props) => <h2 className="text-2xl" {...props} />,
-      h3: (props) => <h3 className="text-xl" {...props} />,
+      h1: Base("h1", "text-center text-3xl"),
+      h2: Base("h2", "text-center text-2xl"),
+      h3: Base("h3", "text-center text-xl"),
+      ul: Base("ul", "list-disc list-inside"),
+      table: Base("table", "table-auto mx-auto"),
+      td: Base("td", "px-4 py-2 border"),
+      th: Base("th", "px-4 py-2 border bg-gray-100"),
     }}
   >
     <MDXContent />
