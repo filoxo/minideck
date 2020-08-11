efd76fd7f062e08ac620e571b289e1f04d1ded7e
Carlos <filoxo@users.noreply.github.com>
2020-08-11 11:42:34 -0600

    Add print styles (#13)

--- components/Slide.js
+++ components/Slide.js
@@ -15,7 +15,7 @@ export default function Slide({ children, isActive, ...props }) {
     <section
       ref={slideRef}
       className={cls(
-        "p-2 w-screen h-screen absolute flex items-center justify-center transition-opacity duration-300 transform",
+        "slide p-2 w-screen h-screen absolute flex items-center justify-center transition-opacity duration-300 transform",
         {
           "opacity-100 translate-y-0": isActive,
           "opacity-0 translate-y-full": !isActive,
