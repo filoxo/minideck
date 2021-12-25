da3277dc461538bccf3121176babebd9c32b8b2a
Carlos Filoteo <c.filoteo12@gmail.com>
2021-12-25 16:17:30 -0700

    Add Appear doc type

--- components/Appear.jsx
+++ components/Appear.jsx
@@ -1,11 +1,15 @@
 // @ts-check
-/*
-Wraps the rendered element in hidden styling.
-The useAppearSequence hook modifies the visibility of this component directly.
-You may also provide an `order` prop to specify the order in which to appear in sequence.
-*/
 import React from "react";
 
+/**
+ * Wraps the rendered element in hidden styling.
+ * The useAppearSequence hook modifies the visibility of this component directly.
+ * You may also provide an `order` prop to specify the order in which to appear in sequence.
+ * 
+ * @param { object } props
+ * @param { boolean } props.order - order in which to appear in sequence
+ * @param { JSX.Element } props.children
+ */
 export default function Appear({ order, ...props }) {
   return (
     <span
