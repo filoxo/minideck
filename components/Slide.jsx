8bc8e823c31e6e348a4389fb92510d32bd75ae7e
Carlos Filoteo <c.filoteo12@gmail.com>
2021-12-25 19:07:12 -0700

    Remove deprecated Tailwind class

--- components/Slide.jsx
+++ components/Slide.jsx
@@ -15,7 +15,7 @@ export default function Slide({ children, isActive, ...props }) {
     <section
       ref={slideRef}
       className={cls(
-        "slide p-2 w-screen h-screen absolute flex items-center justify-center transition-opacity duration-300 transform",
+        "slide p-2 w-screen h-screen absolute flex items-center justify-center transition-opacity duration-300",
         {
           "opacity-100 translate-y-0": isActive,
           "opacity-0 translate-y-full invisible": !isActive,
